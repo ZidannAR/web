d6213814ce87609e2dd26a3b9b19e45096422e86
ZidannAR <zidanalrajib@gmail.com>
2024-09-10 09:02:40 +0700

    buku_tamu

---
+++ index.php
@@ -0,0 +1,10 @@
+ <!-- Begin Page Content -->
+  <?php include_once ('templates/header.php') ?>
+ <div class="container-fluid">
+
+<!-- Page Heading -->
+<h1 class="h3 mb-4 text-gray-800">Blank Page</h1>
+
+</div>
+<!-- /.container-fluid -->
+ <?php include_once ('templates/footer.php') ?>
